f1c7e38cd7ebab6649eb7301cc0539db6b85ff9e
Jerinji2016 <jerinji2016@gmail.com>
2020-02-24 21:38:21 +0530

    Doc & Patient Activity works

--- app/src/main/java/com/dev/emed/DoctorActivity.java
+++ app/src/main/java/com/dev/emed/DoctorActivity.java
@@ -1,14 +1,43 @@
 package com.dev.emed;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DoctorActivity extends AppCompatActivity {
+    TextView dName;
+    TextView dSpecialisation;
+    TextView dID;
 
+    DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("Patient");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
+
+        dName = findViewById(R.id.doc_name_text);
+        dSpecialisation = findViewById(R.id.doc_specialisation_text);
+        dID = findViewById(R.id.doc_reff_id_text);
+
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("count", ""+dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("Firebase Error", ""+databaseError.getDetails());
+            }
+        });
     }
 }
